2caf364ce5c96e7dfb958c31b015171852e57c9b
Баракин Дмитрий Александрович <dab15@europlan.ru>
2016-09-23 11:38:42 +0300

    update

--- src/directives/inn-validator.d.ts
+++
@@ -1,7 +0,0 @@
-import { Validator, FormControl } from '@angular/forms';
-export declare class INNValidator implements Validator {
-    constructor();
-    validate(control: FormControl): {
-        [key: string]: any;
-    };
-}
--- src/directives/ogrn-validator.d.ts
+++
@@ -1,7 +0,0 @@
-import { Validator, FormControl } from '@angular/forms';
-export declare class OGRNValidator implements Validator {
-    constructor();
-    validate(control: FormControl): {
-        [key: string]: any;
-    };
-}
